17fb8a6c809c1d0cc7524cbe1ddfe03cabcbd04f
Alexandrovdotuk <a_alexandrow@icloud.com>
2025-08-19 06:29:21 +0200

    Delete CRM directory

--- CRM/session_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Setze Session-Einstellungen vor session_start()
-ini_set('session.gc_maxlifetime', 2592000); // 30 Tage
-// session.cookie_lifetime wird in auth.php basierend auf Checkbox gesetzt
-?>
--- CRM/test_phpspreadsheet.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-use PhpOffice\PhpSpreadsheet\IOFactory;
-echo "PHPSpreadsheet erfolgreich geladen!";
-?>
